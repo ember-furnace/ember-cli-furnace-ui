9791dc997702fc3f9c5deb79832fa31c747a9245
Eric van Blokland <eric@footsteps.nl>
2015-11-06 15:18:54 +0100

    Fixed Ember 1.12 initializer deprecations

--- app/initializers/furnace-ui.js
+++ app/initializers/furnace-ui.js
@@ -4,22 +4,6 @@ import handleException from 'furnace-ui/utils/handle-exception';
 import UI from 'furnace-ui';
 
 export function initialize(container, application) {
-	var applicationViewFactory = container.lookupFactory('view:application');
-	if(typeof applicationViewFactory==='function') {
-		applicationViewFactory.reopen({
-			didInsertElement : function() {
-				this._super();
-				var service =this.container.lookup('service:window-manager');
-				if(!service.get('hasContainer')) {
-					var windowContainer = this.container.lookup('component:window-container');
-					windowContainer.append();
-				}
-			}
-		});
-	} else {
-		Ember.warn('furnace-ui: Application view is virtual, cannot inject window-container. Make sure you append it in your template.');
-	}
-	
 	
 	application.register('component:window-container', UI.WindowContainer);
 	application.register('service:window-manager', UI.WindowManager);
---
+++ app/instance-initializers/furnace-ui.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import showDialog from 'furnace-ui/utils/dialog-show';
+import handleException from 'furnace-ui/utils/handle-exception';
+import UI from 'furnace-ui';
+
+export function initialize(instance) {
+	var applicationViewFactory = instance.container.lookupFactory('view:application');
+	if(typeof applicationViewFactory==='function') {
+		applicationViewFactory.reopen({
+			didInsertElement : function() {
+				this._super();
+				var service =this.container.lookup('service:window-manager');
+				if(!service.get('hasContainer')) {
+					var windowContainer = this.container.lookup('component:window-container');
+					windowContainer.append();
+				}
+			}
+		});
+	} else {
+		Ember.warn('furnace-ui: Application view is virtual, cannot inject window-container. Make sure you append it in your template.');
+	}	
+};
+
+export default {
+	name: 'furnace-ui',
+	initialize: initialize
+};
