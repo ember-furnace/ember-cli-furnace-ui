b707b77ae539a9131ed7a1a8fbc5e46ff3fca322
Eric van Blokland <eric@footsteps.nl>
2017-05-15 15:20:30 +0200

    Transition from _lookupFactory to factoryFor

--- addon/utils/lookup-factory.js
+++ addon/utils/lookup-factory.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default function lookup(type,object,owner) {
 	owner = owner || getOwner(this);
 	var name=getName(object);
-	var object= owner._lookupFactory(type+':'+name);
+	var object= owner.factoryFor(type+':'+name).class;
 	Ember.assert('furance-ui: Could not resolve dialog '+name,object);
 	if(!object.typeKey) {
 		object.typeKey=name;
